ab8484d9637b866eb18f009e510b1d5fac2c9f35
Babk_ut38@student.itstep.org <Babk_ut38@student.itstep.org>
2024-08-27 18:15:17 +0300

    fix

--- CinemaMinimalAPI/DTO/MovieDTO.cs
+++ CinemaMinimalAPI/DTO/MovieDTO.cs
@@ -1,11 +1,13 @@
-﻿namespace CinemaMinimalAPI.DTO;
+﻿using CinemaMinimalAPI.Models;
+
+namespace CinemaMinimalAPI.DTO;
 
 public class MovieDTO
 {
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public int DurationInMinutes { get; set; }
-    public string Genre { get; set; } = string.Empty;
-    public string Director { get; set; } = string.Empty;
+    public Genre? Genre { get; set; }
+    public Director? Director { get; set; }
 }
 
